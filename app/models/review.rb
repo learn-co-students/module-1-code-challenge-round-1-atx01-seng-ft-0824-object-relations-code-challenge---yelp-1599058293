a0e639ff891e27b1a5b536ef327d029c2841e6b2
ciara-picou <ciarapicou@att.net>
2020-09-02 11:47:39 -0500

    submission for deadline

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,21 @@
 class Review
-  
+  attr_reader :customer, :restaurant
+  attr_accessor :rating
+  # I chose attr_accessor because a 
+  # a customer might change their mind about
+  # the rating and need to overwrite it
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(customer, restaurant, rating)
+    @customer= customer
+    @restaurant= restaurant
+    @rating= rating
+    @@all << self
+  end
 end
+#
