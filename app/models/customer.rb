bb75d0992b220b4afc795f48731460ca440c1bbd
github username <github email address>
2020-09-02 11:37:34 -0500

    submission for deadline

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,43 @@
 class Customer
   attr_accessor :given_name, :family_name
-
+  @@all = []
   def initialize(given_name, family_name)
     @given_name = given_name
     @family_name  = family_name
+    self.class.all << self
   end
-
+#
   def full_name
     "#{given_name} #{family_name}"
   end
+
+  def self.all
+    @@all
+  end
+
+  def restaurants
+    all_rest = Review.all.select {|restaurants| restaurants.customer == self}
+    all_rest.collect {|rest| rest.restaurant.name}.uniq
+  end
+
+  def new_review(restaurant, rating)
+    Review.new(self, restaurant, rating)
+  end
+
+  def num_reviews
+    total = Review.all.select {|reviews| reviews.customer == self}
+    total.count
+  end
+
+  def self.find_by_name(name)
+     first_last = name.split
+     Customer.all.find {|i| i.given_name == first_last[0] && i.family_name == first_last[1]}
+    
+  end
+
+  def self.find_by_given_name(name)
+    Customer.all.select {|names| names.given_name == name}
+  end
+
+  
 end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,22 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+c1 = Customer.new("Jake", "Lira")
+c2 = Customer.new("Jesse", "Rodriguez")
+c3 = Customer.new("Patty", "Sandoval")
+c4 = Customer.new("Jake", "two")
+c5 = Customer.new("Jake", "two")
 
+r1 = Restaurant.new("Dinos")
+r2 = Restaurant.new("Ihop")
+r3 = Restaurant.new("BWW")
+
+rev1 = Review.new(c1, r1, 3.5)
+rev2 = Review.new(c2, r1, 3.8)
+rev7 = Review.new(c2, r1, 4)
+rev3 = Review.new(c3, r2, 4)
+rev4 = Review.new(c1, r3, 4)
+rev5 = Review.new(c2, r3, 4.5)
+rev6 = Review.new(c3, r1, 4.2)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
